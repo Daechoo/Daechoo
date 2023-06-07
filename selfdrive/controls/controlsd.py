050e1b6ba647e53eed6ffc7555be2049a5c3f45f
SangJo Kim <kandrea@nate.com>
2023-06-08 02:29:11 +0900

    fix

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -56,6 +56,7 @@ XState = log.LongitudinalPlan.XState
 LaneChangeState = log.LateralPlan.LaneChangeState
 LaneChangeDirection = log.LateralPlan.LaneChangeDirection
 EventName = car.CarEvent.EventName
+ButtonEvent = car.CarState.ButtonEvent
 ButtonType = car.CarState.ButtonEvent.Type
 SafetyModel = car.CarParams.SafetyModel
 
